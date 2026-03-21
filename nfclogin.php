e4e4c0dc2e4970baf7f13de814c29f5036002c25
AldisKu <aldishtc@gmail.com>
2026-03-22 00:24:34 +0100

    Trim repo to modern components and remove hashed assets

--- nfclogin.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if(session_id() == '') {
-	    	session_start();
-		session_destroy();
-	}
-
-	$arg = $_GET["a"];
-	
-	header('Location: index.html?a=' . $arg);
--- php/utilities/decimaldefs.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-defined('DECIMALSMALL') || define ( 'DECIMALSMALL','DECIMAL (15,2)' );
-defined('DECIMALMIDDLE') || define ( 'DECIMALMIDDLE','DECIMAL (17,2)' );
-defined('DECIMALMIDDLEEXACT') || define ( 'DECIMALMIDDLEEXACT','DECIMAL (17,6)' );
-defined('DECIMALBIG') || define ( 'DECIMALBIG','DECIMAL (19,2)' );
